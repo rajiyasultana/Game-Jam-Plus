e0a33379d6c4343b9dc3115da220ed816fabe48c
Rajiya Sultana <rssumu04@gmail.com>
2025-11-21 21:39:02 +0600

    Added delay panel

--- NULL/Assets/Scripts/GameStarter.cs
+++ NULL/Assets/Scripts/GameStarter.cs
@@ -1,10 +1,45 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameStarter : MonoBehaviour
 {
-    public void StartGame()
+    public Button playButton;
+    public GameObject LiePannel;
+    public TextMeshProUGUI Text;
+    public float delayTIme = 1.5f;
+    public string sceneToLoad = "PlayerController"; 
+
+    public void OnPlayClicked()
     {
-        SceneManager.LoadScene(1);
+        playButton.interactable = false; // Disable spam clicks
+        LiePannel.SetActive(true);
+        StartCoroutine(FunnyLoadingSequence());
     }
+
+    IEnumerator FunnyLoadingSequence()
+    {
+        Text.text = "Loading Fun...";
+        yield return new WaitForSeconds(delayTIme);
+
+        Text.text = "0% Fun found.";
+        yield return new WaitForSeconds(delayTIme);
+
+        Text.text = "Trying again...";
+        yield return new WaitForSeconds(2);
+
+        Text.text = "Still no fun.";
+        yield return new WaitForSeconds(2);
+
+        Text.text = "Ok, 1% fun detected.";
+        yield return new WaitForSeconds(2);
+
+        Text.text = "Fine, starting the NON-fun version.";
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
 }
