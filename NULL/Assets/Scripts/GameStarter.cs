69d1f679b8a76db4f01f28262ee53a27482aae3d
Rajiya Sultana <rssumu04@gmail.com>
2025-11-21 22:39:22 +0600

    Updated Starter Script

--- NULL/Assets/Scripts/GameStarter.cs
+++ NULL/Assets/Scripts/GameStarter.cs
@@ -9,37 +9,65 @@ public class GameStarter : MonoBehaviour
     public Button playButton;
     public GameObject LiePannel;
     public TextMeshProUGUI Text;
-    public float delayTIme = 1.5f;
-    public string sceneToLoad = "PlayerController"; 
+    public Button tryAgainButton;
+
+    public int sceneIndex = 1; 
+
+    private int step = 0;
 
     public void OnPlayClicked()
     {
-        playButton.interactable = false; // Disable spam clicks
+        playButton.interactable = false; 
         LiePannel.SetActive(true);
-        StartCoroutine(FunnyLoadingSequence());
+        StartCoroutine(StartSequence());
     }
 
-    IEnumerator FunnyLoadingSequence()
+    IEnumerator StartSequence()
     {
         Text.text = "Loading Fun...";
-        yield return new WaitForSeconds(delayTIme);
+        yield return new WaitForSeconds(2f);
 
         Text.text = "0% Fun found.";
-        yield return new WaitForSeconds(delayTIme);
+        tryAgainButton.gameObject.SetActive(true);  // show first Try Again button
+    }
 
+    // Called when player presses Try Again
+    public void OnTryAgainPressed()
+    {
+        tryAgainButton.gameObject.SetActive(false);
+
+        if (step == 0)
+        {
+            StartCoroutine(TryAgainStep1());
+        }
+        else if (step == 1)
+        {
+            StartCoroutine(TryAgainStep2());
+        }
+
+        step++;
+    }
+
+    IEnumerator TryAgainStep1()
+    {
         Text.text = "Trying again...";
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(3.0f);
 
         Text.text = "Still no fun.";
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(0.7f);
 
+        tryAgainButton.gameObject.SetActive(true); // show Try Again again
+    }
+
+    IEnumerator TryAgainStep2()
+    {
         Text.text = "Ok, 1% fun detected.";
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1.5f);
 
         Text.text = "Fine, starting the NON-fun version.";
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(3f);
 
-        SceneManager.LoadScene(sceneToLoad);
+        SceneManager.LoadScene(sceneIndex);
     }
 
 }
