4dedc31d5f75afb60d27fe5882de9f40b5196ec2
Mohammad I-K Sakur <mdiftakharkabirsakur@gmail.com>
2025-11-22 10:41:27 +0600

    savesystem

--- NULL/Assets/ScriptableObject/SaveSystem.cs
+++ NULL/Assets/ScriptableObject/SaveSystem.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SaveSystem", menuName = "Scriptable Objects/SaveSystem")]
 public class SaveSystem : ScriptableObject
 {
-    
+    public Transform Player;
+  
+
 }
